0a154d77e1535f93adddf9dea3420f216eda8bed
Payupat <payupat@planetcloud.cloud>
2024-04-23 14:11:26 +0700

    href

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-// window.location.href = "../summary/";
-
-var sub_role = localStorage.getItem("sub_role");
-if (sub_role === "2") {
-  window.location.href = "../electrical/lighting/";
-} else {
-  window.location.href = "../summary/";
-}
+window.location.href = "./summary";
